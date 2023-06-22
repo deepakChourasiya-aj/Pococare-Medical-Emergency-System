a0162fd0234b557c09acac1421c061ebcb9b7935
deepak chourasiya <deepak1812002@gmail.com>
2023-06-22 15:38:55 +0530

    implemented login signup  auth

---
+++ Client/script/navbar.js
@@ -0,0 +1,30 @@
+
+document.querySelector("#navbar").innerHTML = `
+<div id="nav-cont">
+    <div id="hamb">
+        <i class="fa-solid fa-bars"></i>
+    </div>
+    <div data-aos="zoom-out" data-aos-duration="1000" id="nav-logo">
+        <div id="nav-img">
+        LOGO
+        </div>
+    </div>
+    <div data-aos="zoom-out" data-aos-duration="1000" id="nav-menu">
+        <li id="book-app">Book an appointment</li>
+        <li id="find-doc">Find Doctors</li>
+        <li>Departments</li>
+        <li>Security & Help</li>
+    </div>
+    <div data-aos="zoom-out" data-aos-duration="1000" id="nav-user-details">
+
+        <button id="nav-login">Login</button>
+        <button id="nav-reg">Signup</button>    </div>
+</div>
+`
+
+document.getElementById("nav-reg").addEventListener("click",(e)=>{
+    window.location.href = "./html/register.html"
+})
+document.getElementById("nav-login").addEventListener("click",(e)=>{
+    window.location.href = "./html/login.html"
+})
